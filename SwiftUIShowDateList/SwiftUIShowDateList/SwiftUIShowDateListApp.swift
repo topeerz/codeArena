bc4d537632b59efe61676ca031ecb49b5f22dcdd
Jan Maluszynski (External) <jmaluszynski@blackberry.com>
2024-08-21 18:27:32 +0200

    Initial commit of fresh Xcode project.

---
+++ SwiftUIShowDateList/SwiftUIShowDateList/SwiftUIShowDateListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShowDateListApp.swift
+//  SwiftUIShowDateList
+//
+//  Created by topeerz on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShowDateListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIShowDateList/SwiftUIShowDateListTests/SwiftUIShowDateListTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShowDateListTests.swift
+//  SwiftUIShowDateListTests
+//
+//  Created by topeerz on 21/08/2024.
+//
+
+import Testing
+@testable import SwiftUIShowDateList
+
+struct SwiftUIShowDateListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
