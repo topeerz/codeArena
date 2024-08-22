c23f897885d32c079708c512837f34d2795beab3
Jan Maluszynski <topeerz@icloud.com>
2024-08-22 19:14:00 +0200

    Show some dates on the list read from external repo.

--- SwiftUIShowDateList/SwiftUIShowDateList/ContentView.swift
+++ SwiftUIShowDateList/SwiftUIShowDateList/ContentView.swift
@@ -7,15 +7,61 @@
 
 import SwiftUI
 
+struct CurrnetDate: Decodable {
+    let date: String
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+    @State private var currentDates: [CurrnetDate] = []
+
+    private func getDate() async throws -> CurrnetDate? {
+        guard let url = URL(string: "https://aluminum-accidental-tray.glitch.me/testme") else {
+            fatalError("url failed")
         }
-        .padding()
+
+        let (data, response)  = try await URLSession.shared.data(from: url)
+        return try? JSONDecoder().decode(CurrnetDate.self, from: data)
+    }
+
+    private func populateDates() async {
+        currentDates.removeAll()
+        for _ in (1...3) {
+            guard let date = try? await getDate() else {
+                return
+            }
+
+            currentDates.append(date)
+        }
+    }
+
+    private func getDateFor(_ index: Int) -> CurrnetDate? {
+        if currentDates.count > index {
+            return currentDates[index]
+
+        } else {
+            return nil
+        }
+    }
+
+    var body: some View {
+        NavigationView {
+            List(1...3, id: \.self) { index in
+                Text("\(index) \(getDateFor(index - 1)?.date ?? "None")")
+            }.listStyle(.plain)
+
+            .navigationTitle("My List")
+            .navigationBarItems(trailing: Button(action: {
+                Task {
+                    await populateDates()
+                }
+            }, label: {
+                Image(systemName: "arrow.clockwise.circle")
+            }))
+            .task {
+                await populateDates()
+            }
+        }
     }
 }
 
